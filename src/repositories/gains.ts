f3f4ceb27251b65e5d4ba85d826125ec62e59532
Victor Lucas <victorlucas@MacBook-Air-de-Victor.local>
2023-11-25 22:30:59 -0400

    finish project

---
+++ src/repositories/gains.ts
@@ -0,0 +1,13 @@
+export default [
+  { "description": "Salário",     "amount": "1300.52", "type": "entrada", "frequency": "recorrente", "date": "2023-11-10" }, 
+  { "description": "Freela",      "amount": "150.13",  "type": "entrada", "frequency": "eventual",   "date": "2023-11-17" }, 
+  { "description": "Salário",     "amount": "2500.23", "type": "entrada", "frequency": "recorrente", "date": "2023-10-10" }, 
+  { "description": "Freela site", "amount": "900.23",  "type": "entrada", "frequency": "eventual",   "date": "2023-10-21" }, 
+  { "description": "Freela app",  "amount": "950.92",  "type": "entrada", "frequency": "eventual",   "date": "2023-10-23" }, 
+  { "description": "Salário",     "amount": "2500.25", "type": "entrada", "frequency": "recorrente", "date": "2023-09-10" }, 
+  { "description": "Salário",     "amount": "2500.18", "type": "entrada", "frequency": "recorrente", "date": "2023-08-10" }, 
+  { "description": "Salário",     "amount": "2500.15", "type": "entrada", "frequency": "recorrente", "date": "2023-07-10" }, 
+  { "description": "Salário",     "amount": "2500.12", "type": "entrada", "frequency": "recorrente", "date": "2023-06-10" }, 
+  { "description": "Salário",     "amount": "2500.00", "type": "entrada", "frequency": "recorrente", "date": "2023-07-10" },
+  ]
+  
